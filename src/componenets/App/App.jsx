ef2e7bdb1ff5db73726619d48faa6a7125485281
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-02-22 22:29:37 +0200

    full HW

--- src/componenets/App/App.jsx
+++ src/componenets/App/App.jsx
@@ -1,11 +1,19 @@
 // import './App.module.css';
 import Profile from '../Profile/Profile';
 import userData from '../../userData.json';
+import friends from '../../friends.json';
+import transactions from '../../transactions.json';
 import './App.css';
+import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import Task1 from '../Task/Task1';
+import Task2 from '../Task/Task2';
+import Task3 from '../Task/Task3';
 
 const App = () => {
   return (
     <>
+      <Task1 />
       <Profile
         name={userData.username}
         tag={userData.tag}
@@ -13,6 +21,10 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <Task2 />
+      <FriendList friends={friends} />
+      <Task3 />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
